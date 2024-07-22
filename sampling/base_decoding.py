80c3c6b45c86bc497208058c0762dd3e8d46075e
Romain <10656815+romsto@users.noreply.github.com>
2024-07-22 05:03:03 +0000

    improved beam search decoding

--- sampling/base_decoding.py
+++ sampling/base_decoding.py
@@ -112,76 +112,55 @@ def beam_search_generate(
         return ((min_length + length) / (min_length + 1)) ** alpha
 
     prompt_len = len(inputs)
-    # prepare input tensor
     max_seq_length = model.config.max_position_embeddings if hasattr(model.config, 'max_position_embeddings') else (model.config.max_context_length if hasattr(model.config, 'max_context_length') else 1024)
 
     assert prompt_len < max_seq_length, "Prompt length exceeds maximum sequence length."
 
     total_len = min(max_seq_length, prompt_len + max_gen_len)
-    input_ids = torch.full(
-        (num_beams, total_len), pad_token_id, dtype=torch.long, device=model.device
-    )
-    input_ids[:, :prompt_len] = torch.tensor(
-        inputs, dtype=torch.long, device=model.device
-    )
+    input_ids = torch.full((num_beams, total_len), pad_token_id, dtype=torch.long, device=model.device)
+    input_ids[:, :prompt_len] = torch.tensor(inputs, dtype=torch.long, device=model.device)
+    probs = torch.full((num_beams, total_len), torch.finfo(torch.float).min, dtype=torch.float, device=model.device)
+    beams_probs = torch.full((num_beams,), torch.finfo(torch.float).min, dtype=torch.float, device=model.device)
+    last_indexes = torch.full((num_beams,), -1, dtype=torch.long, device=model.device)
 
-    probs = torch.full(
-        (num_beams, total_len), -1.0, dtype=torch.float, device=model.device
-    )
+    stop_tokens = torch.tensor((eos_tokens_id if isinstance(eos_tokens_id, list) else [eos_tokens_id]), dtype=torch.long, device=model.device)
 
-    list_tokens_id = (
-        eos_tokens_id if isinstance(eos_tokens_id, list) else [eos_tokens_id]
-    )
-    stop_tokens = torch.tensor(list_tokens_id, dtype=torch.long, device=model.device)
-
-    # first iteration
-    probs[:, :prompt_len] = 1.0  # fill prompt with 1.0
+    # prefill
+    probs[:, :prompt_len] = 1.0
+    beams_probs[:] = 1.0
     o = model(input_ids[:, :prompt_len])
     curr_prob = torch.nn.functional.log_softmax(o.logits[0, -1, :], dim=-1)
     top_probs, top_tokens = torch.topk(curr_prob, num_beams, dim=-1)
     input_ids[:, prompt_len] = top_tokens
     probs[:, prompt_len] = probs[:, prompt_len - 1] + top_probs
-
+    beams_probs[:] = probs[:, prompt_len] / _length_penalty_fn(1, alpha, min_length)
+    
     for curr in range(prompt_len + 1, total_len):
         o = model(input_ids[:, :curr])
         logits = o.logits[:, -1, :]
         probs_curr = torch.nn.functional.log_softmax(logits, dim=-1)
         top_probs, top_tokens = torch.topk(probs_curr, top_k, dim=-1)
         possibilities = []
-        for i in range(num_beams):
-            if (
-                torch.isin(input_ids[i, curr - 1], stop_tokens)
-                or input_ids[i, curr - 1] == pad_token_id
-            ):
-                # If the beam is already finished, we only add its current state to consideration
-                prob_vec = probs[i].detach().clone()
-                input_vec = input_ids[i].detach().clone()
-                last_token_idx = curr - 1
-                while input_vec[last_token_idx] == pad_token_id:
-                    last_token_idx -= 1
-                lp = _length_penalty_fn(last_token_idx - prompt_len, alpha, min_length)
-                already_in = False
-                for p in possibilities:
-                    if torch.equal(p[1], input_vec):
-                        already_in = True
-                        break
-                if not already_in:
-                    possibilities.append(
-                        (
-                            probs[i, last_token_idx] / (lp if lp != 0 else 1),
-                            input_vec,
-                            prob_vec,
-                        )
-                    )
+        for beam in range(num_beams):
+            if last_indexes[beam] != -1:
+                prob_vec = probs[beam].detach().clone()
+                input_vec = input_ids[beam].detach().clone()
+                possibilities.append(
+                    (beams_probs[beam], input_vec, prob_vec, last_indexes[beam])
+                )
                 continue
-            # Otherwise, we consider the probabilities of all the next possibilities (all beams + top_k tokens)
-            for j in range(top_k):
-                new_prob = probs[i, curr - 1] + top_probs[i, j]
+            
+            for possibility in range(top_k):
+                new_prob = probs[beam, curr - 1] + top_probs[beam, possibility]
                 lp = _length_penalty_fn(curr - prompt_len, alpha, min_length)
-                prob_vec = probs[i].detach().clone()
+                prob_vec = probs[beam].detach().clone()
                 prob_vec[curr] = new_prob
-                input_vec = input_ids[i].detach().clone()
-                input_vec[curr] = top_tokens[i, j]
+                input_vec = input_ids[beam].detach().clone()
+                input_vec[curr] = top_tokens[beam, possibility]
+                last_token_idx = -1
+                if torch.isin(input_vec[curr], stop_tokens) or input_vec[curr] == pad_token_id:
+                    last_token_idx = curr
+                    
                 already_in = False
                 for p in possibilities:
                     if torch.equal(p[1], input_vec):
@@ -189,7 +168,7 @@ def beam_search_generate(
                         break
                 if not already_in:
                     possibilities.append(
-                        (new_prob / (lp if lp != 0 else 1), input_vec, prob_vec)
+                        (new_prob / (lp if lp != 0 else 1), input_vec, prob_vec, last_token_idx)
                     )
 
         possibilities.sort(key=lambda x: x[0], reverse=True)
@@ -199,34 +178,17 @@ def beam_search_generate(
 
         possibilities = possibilities[:num_beams]
 
-        all_stop = True
-        for i in range(num_beams):
-            probs[i] = possibilities[i][2]
-            input_ids[i] = possibilities[i][1]
-            if not (
-                torch.isin(input_ids[i, curr], stop_tokens)
-                or input_ids[i, curr] == pad_token_id
-            ):
-                all_stop = False
+        for beam in range(num_beams):
+            beams_probs[beam] = possibilities[beam][0]
+            input_ids[beam] = possibilities[beam][1]
+            probs[beam] = possibilities[beam][2]
+            last_indexes[beam] = possibilities[beam][3]
 
-        if all_stop:
+        if torch.all(last_indexes != -1):
             if debug:
                 printing.end_token_found(curr)
             break
 
-    # get the best beam
-    best_beam = 0
-    best_beam_end = 0
-    best_prob = -inf
-    for i in range(num_beams):
-        last_token_idx = total_len - 1
-        while input_ids[i, last_token_idx] == pad_token_id:
-            last_token_idx -= 1
-        lp = _length_penalty_fn(last_token_idx - prompt_len, alpha, min_length)
-        corrected_prob = probs[i, last_token_idx] / (lp if lp != 0 else 1)
-        if corrected_prob > best_prob:
-            best_prob = corrected_prob
-            best_beam = i
-            best_beam_end = last_token_idx
+    last_indexes[last_indexes == -1] = total_len - 1
 
-    return input_ids[best_beam, prompt_len : best_beam_end + 1].tolist()
+    return input_ids[0, prompt_len : last_indexes[0] + 1].tolist()
